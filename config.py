4d29b0d1202d82a4cd1d98219f4a0478c2924cb2
smpdmohanty <smpdmohanty@gmail.com>
2024-11-20 23:04:06 -0800

    updates

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
+import json
 
 __all__ = ["config"]
 
 config = {}
 ### check if conf has WLAN detains
 if "config.json" in os.listdir():
-    import json
-
     with open("config.json") as f:
         while jsonline := f.readline():
             cfg = json.loads(jsonline.strip())
@@ -14,3 +13,9 @@ if "config.json" in os.listdir():
     print(json.dumps(config))
 else:
     print("==> config.json not found !")
+
+
+def saveconfig(configdict):
+    with open("config.json") as g:
+        for key, val in configdict.items():
+            g.write(json.dumps({key: val}))
---
+++ lib/accesspoint.py
@@ -0,0 +1,27 @@
+import network
+from config import config
+from socket import socket, SOCK_STREAM, AF_INET
+
+# LOAD CONFIG
+apconf = config["ACCESSPOINT"]
+
+# INIT AP
+ap = network.WLAN(network.AP_IF)
+ap.active(False)
+ap.config(ssid=apconf["ssid"], password=apconf["pass"])
+ap.active(True)  # start AP
+
+html = open("webconfig.html").read()
+# create a web-server to get settings
+def webconfig():
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind(("0.0.0.0",80))
+    sock.listen()
+    conn, addr = sock.accept()
+    conn.sendall(html)
+    return
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(data)
